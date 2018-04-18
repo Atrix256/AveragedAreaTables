7fd64c01acc3faef27c05b89bf231e0e5952fd8c
Alan Wolfe <alan.wolfe@gmail.com>
2018-04-18 06:53:18 -0700

    Box, SAT working. AAT WIP. AAT values are way wrong (bug)

--- Source.cpp
+++ Source.cpp
@@ -9,10 +9,53 @@
 #include "stb_image_write.h"
 
 typedef uint8_t uint8;
+typedef uint16_t uint16;
 typedef uint32_t uint32;
 
-void SATBoxBlur(const std::vector<uint32>& SAT, size_t width, size_t height, size_t radius, std::vector<uint8>& result)
+template <typename T>
+inline T SampleOrZero(T* data, size_t width, size_t height, int x, int y)
 {
+	if (x < 0 || y < 0 || x > width - 1 || y > height - 1)
+		return T(0);
+
+	return data[y*width + x];
+}
+
+void BoxBlur(const uint8* source, size_t width, size_t height, size_t radius, const char* baseFileName)
+{
+	std::vector<uint8> result;
+	result.resize(width * height);
+
+	float filterSize = float(radius * 2 + 1)*float(radius * 2 + 1);
+
+	for (int iy = 0; iy < height; ++iy)
+	{
+		for (int ix = 0; ix < width; ++ix)
+		{
+			float sum = 0;
+
+			for (int filtery = -(int)radius; filtery <= (int)radius; ++filtery)
+			{
+				for (int filterx = -(int)radius; filterx <= (int)radius; ++filterx)
+				{
+					sum += (float)SampleOrZero(source, width, height, ix + filterx, iy + filtery);
+				}
+			}
+
+			result[iy*width + ix] = uint8(0.5f + sum / filterSize);
+		}
+	}
+
+	char append[32];
+	sprintf_s(append, "_%i", int(radius));
+	char fileName[256];
+	sprintf_s(fileName, baseFileName, append);
+	stbi_write_png(fileName, (int)width, (int)height, 1, &result[0], (int)width);
+}
+
+void SATBoxBlur(const std::vector<uint32>& SAT, size_t width, size_t height, size_t radius, const char* baseFileName)
+{
+	std::vector<uint8> result;
 	result.resize(SAT.size());
 
 	for (size_t iy = 0; iy < height; ++iy)
@@ -32,11 +75,63 @@ void SATBoxBlur(const std::vector<uint32>& SAT, size_t width, size_t height, siz
 
 			uint32 integratedValue = A + D - B - C;
 
-			uint8 average = uint8(double(integratedValue) / double((endY - startY)*(endX - startX)));
+			double size = double((endY - startY)*(endX - startX));
+
+			uint8 average = uint8(double(integratedValue) / size);
 
 			result[iy*width + ix] = average;
 		}
 	}
+
+	char append[32];
+	sprintf_s(append, "_%i_SAT", int(radius));
+	char fileName[256];
+	sprintf_s(fileName, baseFileName, append);
+	stbi_write_png(fileName, (int)width, (int)height, 1, &result[0], (int)width);
+}
+
+void AATBoxBlur(const std::vector<uint8>& AAT, const std::vector<uint32>& SAT, size_t width, size_t height, size_t radius, const char* baseFileName)
+{
+	// TODO: remove SAT parameter
+
+	std::vector<uint8> result;
+	result.resize(AAT.size());
+
+	for (size_t iy = 0; iy < height; ++iy)
+	{
+		for (size_t ix = 0; ix < width; ++ix)
+		{
+			size_t startX = (ix < radius + 1) ? 0 : ix - radius - 1;
+			size_t startY = (iy < radius + 1) ? 0 : iy - radius - 1;
+
+			size_t endX = std::min(ix + radius, width - 1);
+			size_t endY = std::min(iy + radius, height - 1);
+
+			uint32 A = uint32(AAT[startY*width + startX]) * uint32((startY + 1)*(startX + 1));
+			uint32 B = uint32(AAT[startY*width + endX]) * uint32((startY + 1)*(endX + 1));
+			uint32 C = uint32(AAT[endY*width + startX]) * uint32((endY + 1)*(startX + 1));
+			uint32 D = uint32(AAT[endY*width + endX]) * uint32((endY + 1)*(startY + 1));
+
+			uint32 SATA = SAT[startY*width + startX];
+			uint32 SATB = SAT[startY*width + endX];
+			uint32 SATC = SAT[endY*width + startX];
+			uint32 SATD = SAT[endY*width + endX];
+
+			uint32 integratedValue = A + D - B - C;
+
+			double size = double((endY - startY)*(endX - startX));
+
+			uint8 average = uint8(double(integratedValue) / size);
+
+			result[iy*width + ix] = average;
+		}
+	}
+
+	char append[32];
+	sprintf_s(append, "_%i_AAT", int(radius));
+	char fileName[256];
+	sprintf_s(fileName, baseFileName, append);
+	stbi_write_png(fileName, (int)width, (int)height, 1, &result[0], (int)width);
 }
 
 void TestAATvsSAT(uint8* source, size_t width, size_t height, const char* baseFileName)
@@ -71,23 +166,19 @@ void TestAATvsSAT(uint8* source, size_t width, size_t height, const char* baseFi
         }
     }
 
-	char fileName[256];
+	size_t radiuses[] = { 1, 5, 25 };
 
-	// box blur with SAT
-	std::vector<uint8> SATBlur1;
-	SATBoxBlur(SAT, width, height, 1, SATBlur1);
-	sprintf_s(fileName, baseFileName, "_1_SAT");
-	stbi_write_png(fileName, (int)width, (int)height, 1, &SATBlur1[0], (int)width);
+	for (size_t index = 0; index < _countof(radiuses); ++index)
+	{
+		// regular box blur
+		//BoxBlur(source, width, height, radiuses[index], baseFileName);
 
-	std::vector<uint8> SATBlur5;
-	SATBoxBlur(SAT, width, height, 5, SATBlur5);
-	sprintf_s(fileName, baseFileName, "_5_SAT");
-	stbi_write_png(fileName, (int)width, (int)height, 1, &SATBlur5[0], (int)width);
+		// box blur with SAT
+		//SATBoxBlur(SAT, width, height, radiuses[index], baseFileName);
 
-	std::vector<uint8> SATBlur25;
-	SATBoxBlur(SAT, width, height, 25, SATBlur25);
-	sprintf_s(fileName, baseFileName, "_25_SAT");
-	stbi_write_png(fileName, (int)width, (int)height, 1, &SATBlur25[0], (int)width);
+		// box blur with AAT
+		AATBoxBlur(AAT, SAT, width, height, radiuses[index], baseFileName);
+	}
 }
 
 int main(int argc, char** argv)
@@ -121,8 +212,9 @@ int main(int argc, char** argv)
 
 /*
 TODO:
-* SAT box blur doesn't look quite right. probably radius calculation is off or something
-* compare to actual box blur of source data!
+* fix box blur edges to look right. eg for regular box blur, maybe divide by the number of pixels that are in range, instead of by filter size always (dont assume it's black around border?).
+ * that would make it like the SAT one
+
 * and box blur using average area tables
 * u8 as source
 * show breakdown of float as well? esp f16 if you can.
