2d08004613e52040d4ff5eae24351cf9f62ff3dd
Alan Wolfe <alan.wolfe@gmail.com>
2018-04-18 20:13:46 -0700

    some cleanup/fixes of AAT and SAT index calculations

--- Source.cpp
+++ Source.cpp
@@ -43,7 +43,7 @@ float AverageOfRectangle(T* data, int width, int height, int sx, int sy, int ex,
     return sum / sampleCount;
 }
 
-void BoxBlur(const uint8* source, size_t width, size_t height, size_t radius, const char* baseFileName)
+void BoxBlur(const uint8* source, int width, int height, int radius, const char* baseFileName)
 {
 	std::vector<uint8> result;
 	result.resize(width * height);
@@ -54,7 +54,7 @@ void BoxBlur(const uint8* source, size_t width, size_t height, size_t radius, co
 	{
 		for (int ix = 0; ix < width; ++ix)
 		{
-            float average = AverageOfRectangle(source, (int)width, (int)height, ix - (int)radius, iy - (int)radius, ix + (int)radius, iy + (int)radius);
+            float average = AverageOfRectangle(source, width, height, ix - radius, iy - radius, ix + radius, iy + radius);
             result[iy*width + ix] = uint8(0.5f + average);
 		}
 	}
@@ -63,27 +63,27 @@ void BoxBlur(const uint8* source, size_t width, size_t height, size_t radius, co
 	sprintf_s(append, "_%i", int(radius));
 	char fileName[256];
 	sprintf_s(fileName, baseFileName, append);
-	stbi_write_png(fileName, (int)width, (int)height, 1, &result[0], (int)width);
+	stbi_write_png(fileName, width, height, 1, &result[0], width);
 }
 
-void SATBoxBlur(const std::vector<uint32>& SAT, size_t width, size_t height, size_t radius, const char* baseFileName)
+void SATBoxBlur(const std::vector<uint32>& SAT, int width, int height, int radius, const char* baseFileName)
 {
 	std::vector<uint8> result;
 	result.resize(SAT.size());
 
-	for (size_t iy = 0; iy < height; ++iy)
+	for (int iy = 0; iy < height; ++iy)
 	{
-		for (size_t ix = 0; ix < width; ++ix)
+		for (int ix = 0; ix < width; ++ix)
 		{
-			size_t startX = (ix < radius + 1) ? 0 : ix - radius - 1;
-			size_t startY = (iy < radius + 1) ? 0 : iy - radius - 1;
+			int startX = std::max(ix - radius - 1, -1);
+			int startY = std::max(iy - radius - 1, -1);
 
-			size_t endX = std::min(ix + radius, width - 1);
-			size_t endY = std::min(iy + radius, height - 1);
+			int endX = std::min(ix + radius, width - 1);
+			int endY = std::min(iy + radius, height - 1);
 
-			uint32 A = SAT[startY*width + startX];
-			uint32 B = SAT[startY*width + endX];
-			uint32 C = SAT[endY*width + startX];
+			uint32 A = (startX >= 0 && startY >= 0) ? SAT[startY*width + startX] : 0;
+			uint32 B = (startY >= 0) ? SAT[startY*width + endX] : 0;
+			uint32 C = (startX >= 0) ? SAT[endY*width + startX] : 0;
 			uint32 D = SAT[endY*width + endX];
 
 			uint32 integratedValue = A + D - B - C;
@@ -100,34 +100,34 @@ void SATBoxBlur(const std::vector<uint32>& SAT, size_t width, size_t height, siz
 	sprintf_s(append, "_%i_SAT", int(radius));
 	char fileName[256];
 	sprintf_s(fileName, baseFileName, append);
-	stbi_write_png(fileName, (int)width, (int)height, 1, &result[0], (int)width);
+	stbi_write_png(fileName, width, height, 1, &result[0], width);
 }
 
-void AATBoxBlur(const std::vector<uint8>& AAT, const std::vector<uint32>& SAT, size_t width, size_t height, size_t radius, const char* baseFileName)
+void AATBoxBlur(const std::vector<uint8>& AAT, const std::vector<uint32>& SAT, int width, int height, int radius, const char* baseFileName)
 {
 	// TODO: remove SAT parameter
 
 	std::vector<uint8> result;
 	result.resize(AAT.size());
 
-	for (size_t iy = 0; iy < height; ++iy)
+	for (int iy = 0; iy < height; ++iy)
 	{
-		for (size_t ix = 0; ix < width; ++ix)
+		for (int ix = 0; ix < width; ++ix)
 		{
 			if (ix == 5 && iy == 5)
 			{
 				int ijkl = 0;
 			}
 
-			size_t startX = (ix < radius + 1) ? 0 : ix - radius - 1;
-			size_t startY = (iy < radius + 1) ? 0 : iy - radius - 1;
+			int startX = std::max(ix - radius - 1, -1);
+			int startY = std::max(iy - radius - 1, -1);
 
-			size_t endX = std::min(ix + radius, width - 1);
-			size_t endY = std::min(iy + radius, height - 1);
+			int endX = std::min(ix + radius, width - 1);
+			int endY = std::min(iy + radius, height - 1);
 
-			uint32 A = uint32(AAT[startY*width + startX]) * uint32((startY + 1)*(startX + 1));
-			uint32 B = uint32(AAT[startY*width + endX]) * uint32((startY + 1)*(endX + 1));
-			uint32 C = uint32(AAT[endY*width + startX]) * uint32((endY + 1)*(startX + 1));
+			uint32 A = (startX >= 0 && startY >= 0) ? uint32(AAT[startY*width + startX]) * uint32((startY + 1)*(startX + 1)) : 0;
+			uint32 B = (startY >= 0) ? uint32(AAT[startY*width + endX]) * uint32((startY + 1)*(endX + 1)) : 0;
+			uint32 C = (startX >= 0) ? uint32(AAT[endY*width + startX]) * uint32((endY + 1)*(startX + 1)) : 0;
 			uint32 D = uint32(AAT[endY*width + endX]) * uint32((endY + 1)*(endX + 1));
 
 			uint32 SATA = SAT[startY*width + startX];
@@ -152,7 +152,7 @@ void AATBoxBlur(const std::vector<uint8>& AAT, const std::vector<uint32>& SAT, s
 	stbi_write_png(fileName, (int)width, (int)height, 1, &result[0], (int)width);
 }
 
-void TestAATvsSAT(uint8* source, size_t width, size_t height, const char* baseFileName)
+void TestAATvsSAT(uint8* source, int width, int height, const char* baseFileName)
 {
     // make SAT
     std::vector<uint32> SAT;
@@ -184,7 +184,8 @@ void TestAATvsSAT(uint8* source, size_t width, size_t height, const char* baseFi
         }
     }
 
-	size_t radiuses[] = { 0, 1, 5, 25, 100 };
+	// TODO: temp
+	int radiuses[] = { 0, 1, 5, 25 };//, 100 };
 
 	for (size_t index = 0; index < _countof(radiuses); ++index)
 	{
